1726f4aeaa68fdf534a39793324534b018fbe82f
David Milton <8712ce@gmail.com>
2025-03-18 17:03:51 -0500

    Started pedestrian model.

--- gameplay/models.py
+++ gameplay/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import random
 
-# Create your models here.
+class Pedestrian(models.Model):
+    name = models.CharField(
+        max_length=100,
+        blank=True, # "reqired: false"
+        null=True
+    )
+    score = models.IntegerField(
+        default=0   # "required: true, default: 0"
+    )
+    image = models.CharField(
+        max_length=225  # "required: true"
+    )
+    is_drunk = models.BooleanField(
+        default=False
+    )
+    bladder = models.IntegerField(
+        default=0
+    )
+    bowels = models.IntegerField(
+        default=0
+    )
